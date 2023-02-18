6256c9824e656183f994ba8e1ffa21220dd0e435
Alexsandr <De_gsrce@mail.ru>
2023-02-18 19:46:02 +0400

    Сделал третью задачу,исправил остольные

--- Reshenie_Zadach_kod/DZ_002/task_002/Program.cs
+++ Reshenie_Zadach_kod/DZ_002/task_002/Program.cs
@@ -2,12 +2,11 @@
 int check2(int meaning)
 {
     int index = 0;
-
     while (6 > index)
     {
         if (meaning / 1000 == 0)
         {
-            index = index + 7;
+            index = index + 11;
         }
         else
         {
@@ -19,15 +18,19 @@ int check2(int meaning)
 }
 void PrintAnswer(int col)
 {
-    int a = col % 100;
-    int b = a % 10;
-    Console.WriteLine(b);
-
+    if (100 > col)
+    {
+        Console.WriteLine("Третьей цифры нету");
+    }
+    else
+    {
+        int first = col % 100;
+        int second = first % 10;
+        Console.WriteLine(second);
+    }
 }
 
 Console.WriteLine("введите любое число до  999 999 999");
 int number = int.Parse(Console.ReadLine()!);
 int result = check2(number);
 PrintAnswer(result);
-
-
---
+++ Reshenie_Zadach_kod/DZ_002/task_003/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+void PrintResult(int wek)
+{
+    string[] day = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+    Console.Write(day[wek - 1]);
+    if (wek == 6 || wek == 7) Console.Write("-УРА ВЫХОДНОЙ");
+}
+
+Console.WriteLine("Введите число от 1 до 7");
+int element = int.Parse(Console.ReadLine()!);
+
+PrintResult(element);
+
