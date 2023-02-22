a0d233a901d2f69b3133722a10820e8abfcd5690
Alexsandr <De_gsrce@mail.ru>
2023-02-22 12:07:44 +0400

    первая задача дз03

--- Reshenie_Zadach_kod/DZ_003/task_001/Program.cs
+++ Reshenie_Zadach_kod/DZ_003/task_001/Program.cs
@@ -1,22 +1,15 @@
 ﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
 void Polindrom(int resault)
 {
-    int[] masiv = new int(5);
+    int[] masiv = new int[5];
     for (int index = 0; index < 5; index++)
     {
-        int[] masiv = resault % 10;
-        Console.WriteLine(masiv[0]);
-
+        masiv[index] = resault % 10;
+        resault = resault / 10;
     }
+    if (masiv[0] == masiv[4] && masiv[1] == masiv[3]) Console.WriteLine("Это полиндром");
+    else Console.WriteLine("Это не полиндром");
 }
-
-
-
-
-
-
-
-
 Console.WriteLine("Введите пятизначное число");
 int number = int.Parse(Console.ReadLine()!);
 Polindrom(number);
