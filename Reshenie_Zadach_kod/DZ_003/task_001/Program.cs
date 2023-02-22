e6226a158e97c1fed233b08af0c3c25fd1228d35
Alexsandr <De_gsrce@mail.ru>
2023-02-22 11:47:31 +0400

    третье дз

---
+++ Reshenie_Zadach_kod/DZ_003/task_001/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+void Polindrom(int resault)
+{
+    int[] masiv = new int(5);
+    for (int index = 0; index < 5; index++)
+    {
+        int[] masiv = resault % 10;
+        Console.WriteLine(masiv[0]);
+
+    }
+}
+
+
+
+
+
+
+
+
+Console.WriteLine("Введите пятизначное число");
+int number = int.Parse(Console.ReadLine()!);
+Polindrom(number);
