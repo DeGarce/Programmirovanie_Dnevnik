34de29c1e7f145ccd7859056853ba7f895453359
Alexsandr <De_gsrce@mail.ru>
2023-03-02 17:05:21 +0400

    Сделал дз4 задание3

---
+++ Reshenie_Zadach_kod/DZ_004/task_003/Program.cs
@@ -0,0 +1,39 @@
+﻿//Напишите метод, который задаёт массив из N элементов и выводит их на экран.
+int GetIntFormUser(string text)
+{
+    int num = 0;
+    bool flag = true;
+    do
+    {
+        Console.Write($"{text}:  ");
+        flag = int.TryParse(Console.ReadLine(), out num);
+    } while (!flag);
+    return num;
+}
+void PrintNumber(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + ",");
+        position++;
+    }
+}
+void FillNumber(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0, 1000);
+        index++;
+    }
+}
+
+
+
+int amount = GetIntFormUser("Введите размер масива");
+int[] masiv = new int[amount];
+FillNumber(masiv);
+PrintNumber(masiv);
