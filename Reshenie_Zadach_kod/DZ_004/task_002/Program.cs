d8db9dad8fda773d2f2f420bddde18e1e6ef06ce
Alexsandr <De_gsrce@mail.ru>
2023-03-02 16:48:59 +0400

    Сделал дз4 задание2

---
+++ Reshenie_Zadach_kod/DZ_004/task_002/Program.cs
@@ -0,0 +1,39 @@
+﻿//Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+int GetIntFormUser(string text)
+{
+    int num = 0;
+    bool flag = true;
+    do
+    {
+        Console.Write($"{text}:  ");
+        flag = int.TryParse(Console.ReadLine(), out num);
+    } while (!flag);
+    return num;
+}
+int Division(int meaning)
+{
+    string variable = meaning.ToString();
+    int value = 0;
+    int resault = 0;
+    for (int i = 0; i < variable.Length; i++)
+    {
+        value = meaning % 10;
+        resault = value + resault;
+        meaning = meaning / 10;
+    }
+    return resault;
+}
+
+int number = GetIntFormUser("Введите число");
+int a = Division(number);
+Console.WriteLine(a);
+
+
+
+
+
+
+
+
+
+
