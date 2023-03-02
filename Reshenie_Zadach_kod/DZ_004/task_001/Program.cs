d9d89c358c5f6983ed4ab1be5cdc404d6979465a
Alexsandr <De_gsrce@mail.ru>
2023-03-02 16:09:42 +0400

    Сделал дз4 задание1

---
+++ Reshenie_Zadach_kod/DZ_004/task_001/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.нельзя использовать Math.Pow
+int GetIntFormUser(string text)
+{
+    int num = 0;
+    bool flag = true;
+    do
+    {
+        Console.Write($"{text}:  ");
+        flag = int.TryParse(Console.ReadLine(), out num);
+    } while (!flag);
+    return num;
+}
+int Exponentiation(int value, int factor)
+{
+    int result = 1;
+    for (int i = 0; i < factor; i++)
+    {
+        result = result * value;
+    }
+    return result;
+}
+
+int number = GetIntFormUser("Введите число");
+int exstent = GetIntFormUser("Введите степень");
+int answer = Exponentiation(number, exstent);
+Console.WriteLine(answer);
