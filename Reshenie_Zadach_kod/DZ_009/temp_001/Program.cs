bea18d39bf10883dcb685b6b2ca258c3835339d8
Alexsandr <De_gsrce@mail.ru>
2023-03-15 21:43:06 +0400

    Сделал дз9 задание1

---
+++ Reshenie_Zadach_kod/DZ_009/temp_001/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+int GetIntFormUser(string text)
+{
+    int num = 0;
+    bool flag = true;
+    do
+    {
+        Console.Write($"{text}:  ");
+        flag = int.TryParse(Console.ReadLine(), out num);
+    } while (!flag);
+    return num;
+}
+void PrintNumber(int collection)
+{
+    if (collection > 0)
+    {
+        Console.WriteLine(collection);
+        PrintNumber(collection - 1);
+    }
+
+}
+
+int amount = GetIntFormUser("Введите число N");
+PrintNumber(amount);
+
+
+
+
+
+
+
+
+
