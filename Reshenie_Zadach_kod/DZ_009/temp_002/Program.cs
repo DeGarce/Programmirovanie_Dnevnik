a533b7783b6fc99899a82aa2fbcb3ce58974baee
Alexsandr <De_gsrce@mail.ru>
2023-03-15 22:23:19 +0400

    Сделал дз9 задание2

---
+++ Reshenie_Zadach_kod/DZ_009/temp_002/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int GetIntFormUser(string text)
+{
+    int num = 0;
+    bool flag = true;
+    do
+    {
+        Console.Write($"{text}:  ");
+        flag = int.TryParse(Console.ReadLine(), out num);
+    } while (!flag);
+    return num;
+}
+int SummaNumber(int min1, int max1)
+{
+    int resault = 0;
+    for (int i = min1; i <= max1; i++)
+    {
+        resault = resault + i;
+    }
+    return resault;
+}
+
+int min = GetIntFormUser("Введите число M");
+int max = GetIntFormUser("Введите число N");
+Console.WriteLine(SummaNumber(min, max));
