59599469248a5543b44a6b2521314caf296ed422
Alexsandr <De_gsrce@mail.ru>
2023-05-08 19:54:41 +0400

    Сделал дз 14 задание 1

---
+++ Reshenie_Zadach_kod/DZ_014/temp_001/temp001.py
@@ -0,0 +1,9 @@
+##Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+def power(number, extent):
+    if (extent == 1):
+        return (number)
+    if (extent != 1):
+        return (number * power(number, extent - 1))
+number = int(input("Введите число: "))
+extent = int(input("Введите его степень: "))
+print("Результат ", power(number, extent))
